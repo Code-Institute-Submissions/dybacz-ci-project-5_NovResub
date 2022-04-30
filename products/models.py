726fdb250db09606e6d65358be560f2b21762891
dybacz <jdybacz@gmail.com>
2022-04-30 18:47:07 +0000

    products app, models and category fixtures added

---
+++ products/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+
+class MainCategory(models.Model):
+    """"""
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class SubCategory(models.Model):
+    """"""
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    """"""
+    main_category = models.ForeignKey(
+        'MainCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    sub_category = models.ManyToManyField(
+        'SubCategory', blank=True)
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
