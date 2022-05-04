a61a4a51fe4f344e672d31272c64544069b076e0
dybacz <jdybacz@gmail.com>
2022-05-04 19:23:18 +0000

    +sort functionality to fitness dropdown

--- products/views.py
+++ products/views.py
@@ -23,19 +23,6 @@ def all_products(request):
         #     main_categories = MainCategory.objects.filter(
         #         name__in=main_categories)
 
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)
-
         if 'main_category' in request.GET and 'sub_category' in request.GET:
             sub_categories = request.GET['sub_category'].split('&&')
             main_categories = request.GET['main_category'].split('&&')
@@ -57,6 +44,19 @@ def all_products(request):
             products = products.filter(main_category__name__in=main_categories)
             main_categories = MainCategory.objects.filter(
                 name__in=main_categories)
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
 
         if 'product_query' in request.GET:
