86173e51a15e9269c2159f63bf97c958fe2871a2
dybacz <jdybacz@gmail.com>
2022-05-04 18:01:36 +0000

    +main_category and sub_category filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, MainCategory, SubCategory
 
 # Create your views here.
 
@@ -11,8 +11,39 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    main_categories = None
+    sub_categories = None
 
     if request.GET:
+        # if 'main_category' in request.GET:
+        #     main_categories = request.GET['main_category'].split('&&')
+        #     products = products.filter(main_category__name__in=main_categories)
+        #     main_categories = MainCategory.objects.filter(
+        #         name__in=main_categories)
+
+        if 'main_category' in request.GET and 'sub_category' in request.GET:
+            sub_categories = request.GET['sub_category'].split('&&')
+            main_categories = request.GET['main_category'].split('&&')
+            products = products.filter(
+                main_category__name__in=main_categories,
+                sub_category__name__in=sub_categories)
+            main_categories = MainCategory.objects.filter(
+                name__in=main_categories)
+            sub_categories = SubCategory.objects.filter(
+                name__in=sub_categories)
+
+        elif 'sub_category' in request.GET:
+            sub_categories = request.GET['sub_category'].split('&&')
+            products = products.filter(sub_category__name__in=sub_categories)
+            sub_categories = SubCategory.objects.filter(
+                name__in=sub_categories)
+        else:
+            main_categories = request.GET['main_category'].split('&&')
+            products = products.filter(main_category__name__in=main_categories)
+            main_categories = MainCategory.objects.filter(
+                name__in=main_categories)
+
+
         if 'product_query' in request.GET:
             query = request.GET['product_query']
             if not query:
@@ -27,6 +58,8 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_main_categories': main_categories,
+        'current_sub_categories': sub_categories,
     }
 
     return render(request, 'products/products.html', context)
