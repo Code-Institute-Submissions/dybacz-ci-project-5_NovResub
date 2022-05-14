0525392a2c03db13067ea838ca01eb81d5865565
dybacz <80072323+dybacz@users.noreply.github.com>
2022-05-14 11:19:00 +0100

    Removed secret key and set debug

--- ci_project_5/settings.py
+++ ci_project_5/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cy72tz!*x*1*65@c%hlv5*u!*$^=%1un-i9mw%ccr!__z4yle!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['peak-fitness.herokuapp.com', 'localhost']
 
