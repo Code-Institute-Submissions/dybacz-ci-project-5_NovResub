e67868873cc1a06e493da66da599384e3d93f4b5
dybacz <jdybacz@gmail.com>
2022-05-14 01:00:52 +0000

    +checkout views and template

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,4 @@
-import uuid #used to generate unite order number
+import uuid  # used to generate unite order number
 
 from django.db import models
 from django.db.models import Sum
@@ -20,9 +20,16 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    delivery_cost = models.DecimalField(
+        max_digits=6, 
+        decimal_places=2, 
+        null=False, default=0)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        null=False, default=0)
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        null=False, default=0)
 
     def _generate_order_number(self):
         """
@@ -37,7 +44,8 @@ class Order(models.Model):
         account for delivery costs.
         """
 
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = (
                 self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE/100)
@@ -45,7 +53,6 @@ class Order(models.Model):
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
-        
 
     def save(self, *args, **kwargs):
         """
@@ -59,12 +66,22 @@ class Order(models.Model):
     def __str__(self):
         return self.order_number
 
+
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
-    quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    """"""
+    order = models.ForeignKey(
+        Order, null=False,
+        blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(
+        max_length=2,
+        null=True, blank=True)  # XS, S, M, L, XL
+    quantity = models.IntegerField(
+        null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False,
+        blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
