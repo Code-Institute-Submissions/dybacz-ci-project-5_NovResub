054ca3e5a00bf40dee63a0f22bfca8dc9991d8ca
dybacz <jdybacz@gmail.com>
2022-07-31 01:32:12 +0000

    fixed returns to for Product Models to only return value as string

--- products/models.py
+++ products/models.py
@@ -2,20 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
 class MainCategory(models.Model):
     """"""
 
     class Meta:
         verbose_name_plural = 'Main Categories'
-    
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.name
-    
+        return str(self.name)
+
     def get_friendly_name(self):
-        return self.friendly_name
+        return str(self.friendly_name)
 
 
 class SubCategory(models.Model):
@@ -28,10 +29,11 @@ class SubCategory(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.name
-    
+        return str(self.name)
+
     def get_friendly_name(self):
-        return self.friendly_name
+        return str(self.friendly_name)
+
 
 class Product(models.Model):
     """"""
@@ -50,4 +52,4 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
