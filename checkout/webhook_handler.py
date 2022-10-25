5054fc5dd2320b510d429af0b0e6a4099811d9b8
dybacz <jdybacz@gmail.com>
2022-10-25 21:42:48 +0000

    + class methods to webhook handler and webhook view

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -12,5 +12,21 @@ class StrpWH_Handler:
         Handles a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
+            content=f'Unhandled Webhook received: {event["type"]}',
+            stats=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handles a the payment_intent.succeeded webhook from Stripe 
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            stats=200)
+
+    def handle_payment_intent_failed(self, event):
+        """
+        Handles a the payment_intent.payment_failed webhook from Stripe 
+        """
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             stats=200)
