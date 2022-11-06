84295d39356c03d34632ccd2068bfdeea700433f
dybacz <jdybacz@gmail.com>
2022-11-06 18:54:33 +0000

    Updated stripe webhook handler to handle profiles and save infor request

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from products.models import Product
 from .models import Order, OrderLineItem
+from profiles.models import UserProfile
 
 import time
 import json
@@ -39,6 +40,21 @@ class StrpWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info is true
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         order_exists = False
         attempts = 1
         while attempts <= 5:
@@ -71,6 +87,7 @@ class StrpWH_Handler:
             try:
                 order = Order.objects.create(
                         full_name=shipping_details.name,
+                        user_profile=profile,
                         email=billing_details.email,
                         phone_number=shipping_details.phone,
                         country=shipping_details.address.country,
