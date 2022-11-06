90defd1eb9b27e0a24f75a3c1cf1144f9896f35e
dybacz <jdybacz@gmail.com>
2022-11-06 14:19:23 +0000

    +profiles view, template & initial model, connected urls and models

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def profile(request):
+    """ Display User Profile """
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
