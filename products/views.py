4c87e7dfb86812ea831193c1e47b7c26baff940a
dybacz <jdybacz@gmail.com>
2022-11-06 23:08:05 +0000

    + editing products view, url and template

--- products/views.py
+++ products/views.py
@@ -120,3 +120,26 @@ def add_product(request):
         'form': form,
     }
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit an exisitng product in store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = AdminProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Success: {product.name} Updated!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, f'Error: Failed to update {product.name}. Please make sure the form is valid')
+    else:
+        form = AdminProductForm(instance=product)
+        messages.info(request, f'Currently Editing: {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+    return render(request, template, context)
