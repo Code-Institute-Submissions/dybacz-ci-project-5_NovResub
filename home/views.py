30458cc90424a2e47dbd35af59da553869498433
dybacz <jdybacz@gmail.com>
2022-11-09 17:00:00 +0000

    + privacy policy page

--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+
+def privacy_policy(request):
+    """ A view to return the index page """
+    return render(request, 'home/privacy_policy.html')
