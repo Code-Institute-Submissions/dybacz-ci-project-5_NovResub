31a0ba3ab1a79b8021f729f7c935f0dd6be9a2d5
dybacz <jdybacz@gmail.com>
2022-11-11 13:50:07 +0000

    + Contact Us & About Us views, templates and urls

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
     path('help/', views.help_page, name='help_page'),
+    path('contact-us/', views.contact_page, name='contact_page'),
+    path('about-us/', views.about_page, name='about_page'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,15 +4,25 @@ from django.shortcuts import render
 
 
 def index(request):
-    """ A view to return the index page """
+    """ A view to return the Index page """
     return render(request, 'home/index.html')
 
 
 def privacy_policy(request):
-    """ A view to return the index page """
+    """ A view to return the Privacy Policy page """
     return render(request, 'home/privacy_policy.html')
 
 
 def help_page(request):
-    """ A view to return the index page """
+    """ A view to return the Help/FAQs page """
     return render(request, 'home/help_page.html')
+
+
+def contact_page(request):
+    """ A view to return the Contact us page """
+    return render(request, 'home/contact_page.html')
+
+
+def about_page(request):
+    """ A view to return the About us page """
+    return render(request, 'home/about_page.html')
