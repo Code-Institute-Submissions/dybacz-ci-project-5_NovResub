f4d97e1d3d643012c8e1da08f0037366bb73ba53
dybacz <jdybacz@gmail.com>
2022-11-12 23:10:00 +0000

    typo correct

--- basket/templatetags/basket_tools.py
+++ basket/templatetags/basket_tools.py
@@ -6,5 +6,5 @@ register = template.Library()
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
-    """ calculates subtotal of multipleof the same item """
+    """ calculates subtotal of multiple of the same item """
     return price * quantity
