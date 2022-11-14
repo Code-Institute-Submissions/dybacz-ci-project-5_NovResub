8ab96fbb9e234d5267f14c3e7c0df573a2226961
dybacz <jdybacz@gmail.com>
2022-11-14 06:28:15 +0000

    css comments added and old classes deleted. css and js formatted

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -24,12 +24,14 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-let card = elements.create('card', {style: style});
+let card = elements.create('card', {
+    style: style
+});
 card.mount('#card-element');
 
 // Handle validaiton errors on the card element in real time
-card.addEventListener('change', function(event) {
-    let errorDiv = document.querySelector('#card-errors')
+card.addEventListener('change', function (event) {
+    let errorDiv = document.querySelector('#card-errors');
     if (event.error) {
         let html = `
             <span class="icon" role="alert">
@@ -52,79 +54,83 @@ form.addEventListener('submit', function (ev) {
         'disabled': true
     });
     let loadingOverlay = document.querySelector('#loading-overlay');
-    loadingOverlay.style.display = "block"
+    loadingOverlay.style.display = "block";
     let cardSubmitBtn = document.querySelector('#submit-button');
     cardSubmitBtn.setAttribute('disabled', true);
     let saveInfoElement = document.querySelector("#id-save-info");
     let saveInfo = !saveInfoElement ? false : saveInfoElement.checked;
     let csrf = document.querySelector('input[name="csrfmiddlewaretoken"]');
-    let csrfToken = csrf.value
+    let csrfToken = csrf.value;
     let postData = {
         'csrfmiddlwaretoken': csrfToken,
         'client_secret': clientSecret,
         'save_info': saveInfo,
-    }
-    let url = '/checkout/cache_checkout_data/'
+    };
+    let url = '/checkout/cache_checkout_data/';
 
     fetch(url, {
         method: "POST",
-        headers: {'Accept': 'application/json' , 'X-Requested-With': 'XMLHttpRequest','X-CSRFToken': csrfToken},
+        headers: {
+            'Accept': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-CSRFToken': csrfToken
+        },
         body: JSON.stringify({
             post_data: postData
         }),
-        }).then(function() {
-            stripe.confirmCardPayment(clientSecret, {
-                payment_method: {
-                    card: card,
-                    billing_details: {
-                        name: form.full_name.value.trim(),
-                        phone: form.phone_number.value.trim(),
-                        email: form.email.value.trim(),
-                        address:{
-                            line1: form.street_address1.value.trim(),
-                            line2: form.street_address2.value.trim(),
-                            city: form.town_or_city.value.trim(),
-                            country: form.country.value.trim(),
-                            state: form.county.value.trim(),
-                        }
-                    }
-                },
-                shipping: {
+    }).then(function () {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
                     name: form.full_name.value.trim(),
                     phone: form.phone_number.value.trim(),
-                    address:{
+                    email: form.email.value.trim(),
+                    address: {
                         line1: form.street_address1.value.trim(),
                         line2: form.street_address2.value.trim(),
                         city: form.town_or_city.value.trim(),
                         country: form.country.value.trim(),
-                        postal_code: form.postcode.value.trim(),
                         state: form.county.value.trim(),
                     }
                 }
-            }).then(function (result) {
-                if (result.error) {
-                    let errorDiv = document.querySelector('#card-errors');
-                    let html = `
+            },
+            shipping: {
+                name: form.full_name.value.trim(),
+                phone: form.phone_number.value.trim(),
+                address: {
+                    line1: form.street_address1.value.trim(),
+                    line2: form.street_address2.value.trim(),
+                    city: form.town_or_city.value.trim(),
+                    country: form.country.value.trim(),
+                    postal_code: form.postcode.value.trim(),
+                    state: form.county.value.trim(),
+                }
+            }
+        }).then(function (result) {
+            if (result.error) {
+                let errorDiv = document.querySelector('#card-errors');
+                let html = `
                         <span class="icon" role="alert">
                             <i class="fas fa-times"></i>
                         </span>
                         <span>${result.error.message}</span>
                     `;
-                    errorDiv.innerHTML = html;
-                    loadingOverlay.style.display = "none"
-                    card.update({
-                        'disabled': false
-                    });
-                    let cardSubmitBtn = document.querySelector('#submit-button');
-                    cardSubmitBtn.removeAttribute('disabled');
-                } else {
-                    if (result.paymentIntent.status === 'succeeded') {
-                        form.submit();
-                    }
+                errorDiv.innerHTML = html;
+                loadingOverlay.style.display = "none";
+                card.update({
+                    'disabled': false
+                });
+                let cardSubmitBtn = document.querySelector('#submit-button');
+                cardSubmitBtn.removeAttribute('disabled');
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
                 }
-            });
-        }).catch(function() {
-            // page reload, error will be picked up by django messages
-            location.reload();
+            }
         });
+    }).catch(function () {
+        // page reload, error will be picked up by django messages
+        location.reload();
+    });
 });
--- products/static/products/js/image_change.js
+++ products/static/products/js/image_change.js
@@ -1,7 +1,7 @@
 let image = document.querySelector('#new_image')
 let filename = document.querySelector('#filename')
 
-image.onchange = function() {
+image.onchange = function () {
     let file = this.files[0];
-    filename.innerHTML = `Image will be set to ${file.name}`
-}
+    filename.innerHTML = `Image will be set to ${file.name}`;
+};
